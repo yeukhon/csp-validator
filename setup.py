1a179198365ad56b285bd95df1ec4a76fadefbd2
Yeuk Hon Wong <yeukhon@acm.org>
2014-05-29 13:23:47 -0400

    Update setup.py with some classifiers.
    This is a 2nd rewrite of the original csp-validator, so the new setup.py didn't have all the metadata I want it to have. Now I copy and paste what I have from the old csp-validator source code.

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="csp_validator",
-    version="0.0",
-    description="Content Security Policy validator in Python",
-    packages=['csp_validator'],
+setup(name="csp-validator",
+      version="0.3",
+      description="Content-Security-Policy validator",
+      author="Yeuk Hon Wong",
+      classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Security',
+      ],
+      author_email="yeukhon@acm.org",
+      packages=find_packages(),
 )
